721c2666b0e44b4284a2a9bc88afedaf99a83def
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2018-09-11 18:41:42 -0400

    If brew is not installed, the script will now install it

--- zsh-install-mac.sh
+++ zsh-install-mac.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 echo This script will install zsh, oh-my-zsh, and some zsh plugins that I use.
-echo Assumes you have brew installed
+
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 brew install zsh 
-
 echo now changing shell to zsh
 chsh -s $(which zsh)
+
 echo installing oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo installing powerline fonts
@@ -17,14 +21,17 @@ cd fonts
 # clean-up a bit
 cd ..
 rm -rf fonts
-echo make sure to set iterm font to hack
-echo now installing zsh-autosuggestions
+echo "make sure to set iterm font to hack"
+
+echo "now installing zsh-autosuggestions"
 env -i git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo zsh-autosuggestions installed
-echo installing zsh-syntax-highlighting
+echo "zsh-autosuggestions installed"
+
+echo "installing zsh-syntax-highlighting"
 brew install zsh-syntax-highlighting
-echo installed zsh-syntax-highlighting
-echo copying .zshrc to ~/.zshrc 
+echo "installed zsh-syntax-highlighting"
+
+echo "copying .zshrc to ~/.zshrc" 
 cp ./.zshrc ~/
-echo done
-echo restart your terminal to see changes
+echo "done"
+echo "restart your terminal to see changes"
